2e0d631ae907eeb308cd6b1c918d7ac816bb43dd
jacando.tos <sereicikas.t@gmail.com>
2020-01-24 09:05:14 +0100

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   entry: ["./src/js/index.js"],
   output: { path: path.resolve(__dirname, "dist"), filename: "js/bundle.js" },
   devServer: { contentBase: "./dist" },
-  plugins: [new HtmlWebpackPlugin({ filename: "index.html", template: "./src/index.html" })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      }
+    ]
+  }
 };
