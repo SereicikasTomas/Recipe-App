eeb8024fd91d68d786048cc9fa7d9c882b19761b
jacando.tos <sereicikas.t@gmail.com>
2020-02-04 08:40:30 +0100

    add global state

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
 import Search from "./models/Search";
 
-const search = new Search("pizza");
+/**
+ * Global state of the object
+ * - Search object
+ * - Current reciipe object
+ * - Shoping list object
+ * - Liked recipes object
+ */
+const state = {};
 
-console.log(search);
+const controlSearch = async () => {
+  // 1. Ger query from thee view
+  const query = "pizza";
 
-search.getRecipes();
+  if(query) {
+    // 2. New search object and add too state
+    state.search = new Search(query);
+
+    // 3. Prepare Ui for the result
+
+    // 4. Search for recipes
+    await state.search.getRecipes();
+
+    // 5. Render results on the UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
