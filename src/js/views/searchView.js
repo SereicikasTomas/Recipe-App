bda89a108be423821a6d2a32b3b11f8bd734427d
jacando.tos <sereicikas.t@gmail.com>
2020-02-11 09:43:10 +0100

    add item render functions

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,1 +1,24 @@
 import { elements } from "./base";
+
+export const getInput = () => elements.searchInput.value;
+
+const renderRecipe = recipe => {
+    const { recipe_id, image_url, tittle, publisher } = recipe;
+    const markup = `
+        <li>
+            <a class="results__link results__link--active" href="#${recipe_id}">
+                <figure class="results__fig">
+                    <img src="${image_url}" alt="${tittle}">
+                </figure>
+                <div class="results__data">
+                <h4 class="results__name">${tittle}</h4>
+                <p class="results__author">${publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+};
+
+export const renderResults = recipes => {
+    recipes.forEach(renderRecipe);
+};
