5da8a8f36f8990d99525e3c8eab9adddd08a063d
jacando.tos <sereicikas.t@gmail.com>
2020-02-17 08:59:55 +0100

    add function t trim the tittle

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,21 @@ export const clearInput = () => { elements.searchInput.value = "" };
 
 export const clearResults = () => { elements.searchResList.innerHTML = "" };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(" ").reduce((acc, cur) => {
+            if(acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur;
+        }, 0);
+
+        return `${newTitle.join(" ")} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const { recipe_id, image_url, title, publisher } = recipe;
     const markup = `
@@ -15,7 +30,7 @@ const renderRecipe = recipe => {
                     <img src="${image_url}" alt="${title}">
                 </figure>
                 <div class="results__data">
-                <h4 class="results__name">${title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(title)}</h4>
                 <p class="results__author">${publisher}</p>
                 </div>
             </a>
