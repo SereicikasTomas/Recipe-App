b1b0e77f761f22cef78ef19b8df3fd9d8d6e7b4a
jacando.tos <sereicikas.t@gmail.com>
2020-02-17 07:41:50 +0100

    add search view items

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,20 +3,22 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 const renderRecipe = recipe => {
-    const { recipe_id, image_url, tittle, publisher } = recipe;
+    const { recipe_id, image_url, title, publisher } = recipe;
     const markup = `
         <li>
             <a class="results__link results__link--active" href="#${recipe_id}">
                 <figure class="results__fig">
-                    <img src="${image_url}" alt="${tittle}">
+                    <img src="${image_url}" alt="${title}">
                 </figure>
                 <div class="results__data">
-                <h4 class="results__name">${tittle}</h4>
+                <h4 class="results__name">${title}</h4>
                 <p class="results__author">${publisher}</p>
                 </div>
             </a>
         </li>
     `;
+
+    elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
 export const renderResults = recipes => {
