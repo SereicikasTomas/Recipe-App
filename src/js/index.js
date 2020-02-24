9bd733409aa5600c9b83eb3a0376749fbca3370c
jacando.tos <sereicikas.t@gmail.com>
2020-02-24 10:16:47 +0100

    adjust index for buttons

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ const controlSearch = async () => {
   // 1. Ger query from thee view
   const query = searchView.getInput();
 
-  if(query) {
+  if (query) {
     // 2. New search object and add too state
     state.search = new Search(query);
 
@@ -37,3 +37,12 @@ elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+})
